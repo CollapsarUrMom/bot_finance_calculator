b9f3d30310003b941c4fc00154419622c8e54a90
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-07-15 19:12:52 +0300

    Минимальный бот и библиотеки к нему

---
+++ app.py
@@ -0,0 +1,49 @@
+# Чтобы актвировать виртуальное окружение нужно в Powershell ввести команду (под правами администратора)
+# Set-ExecutionPolicy Unrestricted
+
+# Что бы отключить эту настройку нужно ввести
+# Set-ExecutionPolicy Restricted
+# Это будет стандартная настройка Powershell
+
+# Создание виртуального окружения
+# python -m venv env
+
+# Активация виртуального окружения
+# env\Scripts\activate
+
+# Установка пакетов в виртуальное окружение
+# pip install starlette
+
+# Удаление пакетов в виртуальном окружении
+# pip uninstall starlette
+
+# Деактивация виртуального окружения
+# deactivate
+
+# ====================================================================
+
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+# from .gitignore import TOKEN
+
+bot = Bot(token='TOKEN')
+dp = Dispatcher()
+
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Это команда старт')
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+asyncio.run(main())
