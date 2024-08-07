2378b0e5d9e5efdab60d2eebf9a14bdb041785ae
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-08-07 05:53:15 +0300

    Файл с роутером отлавливающем сообщения от пользователя

--- handlers/handlers_user.py
+++ handlers/handlers_user.py
@@ -1,10 +1,18 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart, Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 from kbds import user_kbds
+from common import bot_common_list
 
 
 user_router = Router()
+
+
+class Text_handler(StatesGroup):
+    text_input = State()
+
 
 
 @user_router.message(CommandStart())
@@ -14,4 +22,23 @@ async def start_cmd(message: types.Message):
 
 @user_router.message(Command('menu'))
 async def user_menu(message: types.Message):
-    await message.answer('Это то что я умею', reply_markup= user_kbds.menu_kb)
+    await message.reply('Это то что я умею', reply_markup= user_kbds.menu_kb)
+    print('Появилась клавиатура')
+
+
+@user_router.message(F.text == 'new')
+async def user_menu(message: types.Message):
+    await message.reply('Заработало!')
+
+
+@user_router.message(StateFilter(None), F.text == 'Er')
+async def task_formation(message: types.Message, state: FSMContext):
+    await state.set_state(Text_handler.text_input)
+    await message.answer('Вставте необходимый текст')
+
+
+@user_router.message(Text_handler.text_input, F.text)
+async def func_input_text(message: types.Message, state: FSMContext):
+    await state.update_data(input_text= message.text)
+    await message.answer(await bot_common_list.revers_text(await state.get_data()))
+    await state.clear()
