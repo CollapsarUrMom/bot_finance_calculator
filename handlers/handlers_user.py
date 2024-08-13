15b4bd1d64818283cfc7cbcd2a037f3ec315d044
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-08-13 21:23:16 +0300

    Убрал ненужные хэндлеры, добавил клавиатуру для /menu и протестировал кнопку отмена

--- handlers/handlers_user.py
+++ handlers/handlers_user.py
@@ -3,42 +3,43 @@ from aiogram.filters import CommandStart, Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-from kbds import user_kbds
+from kbds.user_kbds import get_keyboard
 from common import bot_common_list
 
 
 user_router = Router()
 
 
-class Text_handler(StatesGroup):
+class Snake_text(StatesGroup):
     text_input = State()
 
 
 
+@user_router.message(Command('menu'))
 @user_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Привет пока что я умею только обрабатывать текст, вызови команду меню')
+    await message.answer('Привет пока что я умею только обрабатывать текст, вызови команду меню',
+                        reply_markup=get_keyboard(
+                            'Добавить',
+                            'Отмена',
+                            placeholder='Вставте json',
+                            sizes=(2,),
+                        ))
 
 
-@user_router.message(Command('menu'))
-async def user_menu(message: types.Message):
-    await message.reply('Это то что я умею', reply_markup= user_kbds.menu_kb)
-    print('Появилась клавиатура')
-
-
-@user_router.message(F.text == 'new')
-async def user_menu(message: types.Message):
-    await message.reply('Заработало!')
-
-
-@user_router.message(StateFilter(None), F.text == 'Er')
+@user_router.message(StateFilter(None), Command('snake'))
 async def task_formation(message: types.Message, state: FSMContext):
-    await state.set_state(Text_handler.text_input)
+    await state.set_state(Snake_text.text_input)
     await message.answer('Вставте необходимый текст')
 
 
-@user_router.message(Text_handler.text_input, F.text)
+@user_router.message(Snake_text.text_input)
 async def func_input_text(message: types.Message, state: FSMContext):
     await state.update_data(input_text= message.text)
     await message.answer(await bot_common_list.revers_text(await state.get_data()))
     await state.clear()
+
+
+@user_router.message(F.text == 'Отмена')
+async def func_input_text(message: types.Message, state: FSMContext):
+    await message.answer('Сработал', reply_markup=types.ReplyKeyboardRemove())
