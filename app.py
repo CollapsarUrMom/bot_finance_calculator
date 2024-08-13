be57bf0278c0278899f720648862370ae54bbde3
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-08-13 22:34:23 +0300

    Создал базу данных

--- app.py
+++ app.py
@@ -57,18 +57,37 @@ from aiogram import Bot, Dispatcher
 
 from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
+
 from handlers.handlers_user import user_router
 
-load_dotenv(find_dotenv())
+from middlewares.db import DataBaseSession
+
+from database.engine import create_db, drop_db, session_maker
+
+
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-
 dp.include_router(user_router)
+
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+    await create_db()
+
+async def on_shutdown(bot):
+    print('бот лег')
+
 
 
 async def main():
-    await dp.start_polling(bot)
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+    await bot.delete_webhook(drop_pending_updates=True) # Сброс ожидаемых обновлений
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
---
+++ database/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import DateTime, Float, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    quantity:Mapped[int] = mapped_column()
