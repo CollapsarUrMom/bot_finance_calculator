ef8ea8a631a055195d51bfd730ac80f55a069d2e
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-08-30 16:39:44 +0300

    Добавил нужные столбцы в таблицу базы данных

--- database/models.py
+++ database/models.py
@@ -9,7 +9,9 @@ class Base(DeclarativeBase):
 class Product(Base):
     __tablename__ = 'product'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), primary_key= True, nullable=False)
     price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
     quantity:Mapped[int] = mapped_column()
+    suma:Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    time:Mapped[int] = mapped_column()
