7fd6a06f920cc6eb3af9e498fb12909f55622462
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-08-31 16:34:49 +0300

    Добавил кнопку меню

--- app.py
+++ app.py
@@ -53,16 +53,15 @@
 
 import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
+from common.bot_common_list import private
 from handlers.handlers_user import user_router
-
 from middlewares.db import DataBaseSession
-
 from database.engine import create_db, drop_db, session_maker
 
 
@@ -88,6 +87,8 @@ async def main():
     dp.shutdown.register(on_shutdown)
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
     await bot.delete_webhook(drop_pending_updates=True) # Сброс ожидаемых обновлений
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats()) # Удаление кнопки меню
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats()) # Сама кнопка меню
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
